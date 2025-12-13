052ae225228456b35ee30177a491588f8c83cd11
fsanchesgi <133274266+fsanchesgi@users.noreply.github.com>
2025-12-13 13:07:17 -0300

    Update Program.cs

--- FiqueBellaFinal/Program.cs
+++ FiqueBellaFinal/Program.cs
@@ -7,19 +7,14 @@ builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+// Serve front-end estático
+app.UseDefaultFiles();
+app.UseStaticFiles();
 
-app.UseHttpsRedirection();
-
+app.UseRouting();
 app.UseAuthorization();
 
 app.MapControllers();
