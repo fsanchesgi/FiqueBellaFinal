e940ebfbe9499975427d173b3081b7560300aca6
fsanchesgi <133274266+fsanchesgi@users.noreply.github.com>
2025-12-13 18:08:14 -0300

    Update AdminCategoriaController.cs

--- FiqueBellaFinal/Areas/Admin/Controllers/AdminCategoriaController.cs
+++ FiqueBellaFinal/Areas/Admin/Controllers/AdminCategoriaController.cs
@@ -1,11 +1,7 @@
-using Microsoft.AspNetCore.Mvc;                  // Para Controller, IActionResult, etc.
-using Microsoft.AspNetCore.Authorization;        // Para [Authorize]
-using FiqueBellaFinal.Data;                       // Para AppDbContext
-using FiqueBellaFinal.Services;                   // Para RelatorioServices, GraficoServices, etc.
-using Microsoft.Extensions.Options;              // Para IOptions<>
-using FiqueBellaFinal.Models;                     // Para modelos se usados
-using FiqueBellaFinal.Repositories.Interfaces;   // Para repositórios se usados
-// se usar IOptions<>
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using FiqueBellaFinal.Data;
+using FiqueBellaFinal.Models;
 
 
 
