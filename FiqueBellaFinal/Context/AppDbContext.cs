1ef50bf41965219b813222c0afa2a15a93f0878d
fsanchesgi <133274266+fsanchesgi@users.noreply.github.com>
2025-12-13 14:39:35 -0300

    Update AppDbContext.cs

--- FiqueBellaFinal/Context/AppDbContext.cs
+++ FiqueBellaFinal/Context/AppDbContext.cs
@@ -1,12 +1,22 @@
 using Microsoft.EntityFrameworkCore;
 using FiqueBellaFinal.Models;
 
-namespace FiqueBellaFinal.Data
+namespace FiqueBellaFinal.Context
 {
     public class AppDbContext : DbContext
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Procedimento> Procedimentos { get; set; }
+        public DbSet<Contabilidade> Contabilidades { get; set; }
+        public DbSet<Cliente> Clientes { get; set; }
+        public DbSet<Agenda> Agendas { get; set; }
+        public DbSet<Sugestao> Sugestaos { get; set; }
+        public DbSet<Tipo> Tipos { get; set; }
         public DbSet<Produto> Produtos { get; set; }
+        public DbSet<EntradaSaida> EntradasSaidas { get; set; }
+
+        // Adicione outros DbSet conforme necessário
     }
 }
