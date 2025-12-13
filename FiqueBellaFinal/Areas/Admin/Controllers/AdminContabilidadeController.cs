fe21be7e7206865aa223958d4e70e442794f1bea
fsanchesgi <133274266+fsanchesgi@users.noreply.github.com>
2025-12-13 17:32:06 -0300

    Update AdminContabilidadeController.cs

--- FiqueBellaFinal/Areas/Admin/Controllers/AdminContabilidadeController.cs
+++ FiqueBellaFinal/Areas/Admin/Controllers/AdminContabilidadeController.cs
@@ -1,13 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
-using Microsoft.EntityFrameworkCore;
-using FiqueBellaFinal.Data;
 using FiqueBellaFinal.Models;
-using FiqueBellaFinal.Areas.Admin.Services;
+using FiqueBellaFinal.Repositories.Interfaces;
+
 
 namespace FiqueBellaFinal.Areas.Admin.Controllers
 {
