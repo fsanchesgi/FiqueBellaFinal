ef0adce3f058203e7f8d8ca86a4466b3ace4d78c
fsanchesgi <133274266+fsanchesgi@users.noreply.github.com>
2025-12-14 07:11:40 -0300

    Update ProcedimentoListViewModel.cs

--- FiqueBellaFinal/ViewModels/ProcedimentoListViewModel.cs
+++ FiqueBellaFinal/ViewModels/ProcedimentoListViewModel.cs
@@ -1,11 +1,12 @@
 ﻿using FiqueBellaFinal.Models;
 using System.IO;
+using System.Collections.Generic;
 
 namespace FiqueBellaFinal.ViewModels
 {
     public class ProcedimentoListViewModel
     {
-        public IEnumerable<Procedimento> Procedimentos { get; set; }
-        public string CategoriaAtual { get; set; }
+        public IEnumerable<Procedimento> Procedimentos { get; set; } = new List<Procedimento>();
+        public string CategoriaAtual { get; set; } = string.Empty;
     }
 }
