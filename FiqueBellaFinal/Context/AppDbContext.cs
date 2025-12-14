abfa4cbe5283fd35e82d8268170261180959a736
fsanchesgi <133274266+fsanchesgi@users.noreply.github.com>
2025-12-14 08:27:30 -0300

    Update AppDbContext.cs

--- FiqueBellaFinal/Context/AppDbContext.cs
+++ FiqueBellaFinal/Context/AppDbContext.cs
@@ -1,7 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using FiqueBellaFinal.Models;
 
-namespace FiqueBellaFinal.Context
+namespace FiqueBellaFinal.Data
 {
     public class AppDbContext : DbContext
     {
