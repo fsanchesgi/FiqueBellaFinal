b49cb5e929b0b0ba3a9ef676b6ce4c318414e6df
fsanchesgi <133274266+fsanchesgi@users.noreply.github.com>
2025-12-14 08:41:23 -0300

    Update Produto.cs

--- FiqueBellaFinal/Models/Produto.cs
+++ FiqueBellaFinal/Models/Produto.cs
@@ -1,9 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace FiqueBellaFinal.Models
 {
     public class Produto
     {
         public int Id { get; set; }
+
+        [Required]
         public string Nome { get; set; }
+
+        [Column(TypeName = "decimal(18,2)")]
         public decimal Preco { get; set; }
+
+        public int CategoriaId { get; set; }
+        public Categoria Categoria { get; set; }
     }
 }
