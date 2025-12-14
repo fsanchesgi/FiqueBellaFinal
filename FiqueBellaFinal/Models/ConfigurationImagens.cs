dc38d248b05c9e2f16ab6ec7b5afe038a348a6dc
fsanchesgi <133274266+fsanchesgi@users.noreply.github.com>
2025-12-14 08:01:31 -0300

    Update ConfigurationImagens.cs

--- FiqueBellaFinal/Models/ConfigurationImagens.cs
+++ FiqueBellaFinal/Models/ConfigurationImagens.cs
@@ -1,5 +1,8 @@
-﻿namespace FiqueBellaFinal.Models
+using Microsoft.EntityFrameworkCore;
+
+namespace FiqueBellaFinal.Models
 {
+    [Keyless] // ✅ ADICIONADO
     public class ConfigurationImagens
     {
         public string NomePastaImagensAtendimentos { get; set; }
