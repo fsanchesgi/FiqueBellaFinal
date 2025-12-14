09691c1d6b24ca52dc4a726cdf308f3b6b29f248
fsanchesgi <133274266+fsanchesgi@users.noreply.github.com>
2025-12-14 07:51:38 -0300

    Update FileManagerModel.cs

--- FiqueBellaFinal/Models/FileManagerModel.cs
+++ FiqueBellaFinal/Models/FileManagerModel.cs
@@ -1,10 +1,21 @@
-﻿namespace FiqueBellaFinal.Models
+using Microsoft.AspNetCore.Http;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FiqueBellaFinal.Models
 {
     public class FileManagerModel
     {
+        [NotMapped] // <- EF Core irá ignorar esta propriedade
         public FileInfo[] Files { get; set; }
+
+        [NotMapped] // <- EF Core também ignora tipos não mapeáveis
         public IFormFile IFormFile { get; set; }
+
+        [NotMapped]
         public List<IFormFile> IFormFiles { get; set; }
-        public string PathImagesAtendimento { get; set; }
+
+        public string PathImagesAtendimento { get; set; } // essa sim será mapeada no banco
     }
 }
